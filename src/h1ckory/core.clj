f797ad3e04f223e3e65e0a9acdd19dd4ac073f18
Caleb <beerscb@gmail.com>
2021-08-06 14:17:21 -0500

    add Guardian scraper function

--- src/h1ckory/core.clj
+++ src/h1ckory/core.clj
@@ -9,11 +9,36 @@
 
 (defn get-html 
   [url]
-  (get (client/get "http://google.com") :body))
+  (get (client/get url) :body))
 
-(defn hicthis
+(defn hickory-this
+  [html]
+  (as-hickory (parse html)))
+
+(defn hiccup-this
   [html]
   (as-hiccup (parse html)))
+
+(defn url-to-hickory
+  [url]
+  (->
+    (get-html url)
+    (hickory-this)))
+
+(defn url-to-hiccup
+  [url]
+  (->
+    (get-html url)
+    (hiccup-this)))
+
+(defn retrieve-from-guardian
+  [url]
+  (s/select
+    (s/id "maincontent")
+    (url-to-hickory url)))
+
+
+;;The next step is to add something between hic-this and retrieve-content to find stuff based on tag name/id/class. Read about map, filter, reduce, and apply in the clojure docs.
 
 (defn retrieve-content
   [hicvec]
