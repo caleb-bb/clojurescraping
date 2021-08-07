dad748371a4dabe5654053f67256ef27ec35b0e8
Matt Laine <matt@brain-fuel.co>
2021-08-06 20:44:15 -0400

    remove swap file and run cljfmt

--- src/h1ckory/core.clj
+++ src/h1ckory/core.clj
@@ -1,13 +1,13 @@
 (ns h1ckory.core)
 
-(require '[hickory.select :as s])
 (require '[clj-http.lite.client :as client])
 (require '[clojure.string :as string])
+(require '[hickory.select :as s])
 
-(use 'hickory.core)
 (use 'hiccup.core)
+(use 'hickory.core)
 
-(defn get-html 
+(defn get-html
   [url]
   (get (client/get url) :body))
 
@@ -22,53 +22,48 @@
 (defn url-to-hickory
   [url]
   (->
-    (get-html url)
-    (hickory-this)))
+   (get-html url)
+   (hickory-this)))
 
 (defn url-to-hiccup
   [url]
   (->
-    (get-html url)
-    (hiccup-this)))
+   (get-html url)
+   (hiccup-this)))
 
 (defn retrieve-from-guardian
   [url]
   (s/select
-    (s/id "maincontent")
-    (url-to-hickory url)))
+   (s/id "maincontent")
+   (url-to-hickory url)))
 
 ;this one also works for the NY Post
 (defn retrieve-from-federalist
   [url]
   (s/select
-    (s/class "entry-content")
-    (url-to-hickory url)))
-
+   (s/class "entry-content")
+   (url-to-hickory url)))
 
 (defn retrieve-from-vox
   [url]
   (s/select
-    (s/class "c-entry-content")
-    (url-to-hickory url)))
+   (s/class "c-entry-content")
+   (url-to-hickory url)))
 
 ;re-implement this with a map later
 (defn guardian-retrieve-fragment
   [hickory]
-    (if (= (type hickory) "clojure.lang.PersistentVector") (do guardian-retrieve-fragment (first hickory)))
-    (if (= (type hickory) "clojure.lang.PersistentArrayMap") (do guardian-retrieve-fragment (get hickory :content)) hickory))
+  (if (= (type hickory) "clojure.lang.PersistentVector") (do guardian-retrieve-fragment (first hickory)))
+  (if (= (type hickory) "clojure.lang.PersistentArrayMap") (do guardian-retrieve-fragment (get hickory :content)) hickory))
 
 (defmacro retrieve-fragment
   [url identifier value]
   (list s/select
-    (list identifier value)
-    (list url-to-hickory url)))
+        (list identifier value)
+        (list url-to-hickory url)))
 
 ;;The next step is to add something between hic-this and retrieve-content to find stuff based on tag name/id/class. Read about map, filter, reduce, and apply in the clojure docs.
 
 (defn retrieve-content
   [hicvec]
   (-> (flatten hicvec) (last)))
-
-
-
-
