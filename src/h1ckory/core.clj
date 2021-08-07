b7ce55793f062dc6c31057c66b4ab4e335ff4352
Matt Laine <matt@brain-fuel.co>
2021-08-07 11:20:20 -0400

    clean up threading macros and function param location

--- src/h1ckory/core.clj
+++ src/h1ckory/core.clj
@@ -7,63 +7,52 @@
 (use 'hiccup.core)
 (use 'hickory.core)
 
-(defn get-html
-  [url]
+(defn get-html [url]
   (get (client/get url) :body))
 
-(defn hickory-this
-  [html]
+(defn hickory-this [html]
   (as-hickory (parse html)))
 
-(defn hiccup-this
-  [html]
+(defn hiccup-this [html]
   (as-hiccup (parse html)))
 
-(defn url-to-hickory
-  [url]
-  (->
-   (get-html url)
-   (hickory-this)))
+(defn url-to-hickory [url]
+  (-> url
+      (get-html)
+      (hickory-this)))
 
-(defn url-to-hiccup
-  [url]
-  (->
-   (get-html url)
-   (hiccup-this)))
+(defn url-to-hiccup [url]
+  (-> url
+      (get-html)
+      (hicup-this)))
 
-(defn retrieve-from-guardian
-  [url]
+(defn retrieve-from-guardian [url]
   (s/select
    (s/id "maincontent")
    (url-to-hickory url)))
 
 ;this one also works for the NY Post
-(defn retrieve-from-federalist
-  [url]
+(defn retrieve-from-federalist [url]
   (s/select
    (s/class "entry-content")
    (url-to-hickory url)))
 
-(defn retrieve-from-vox
-  [url]
+(defn retrieve-from-vox [url]
   (s/select
    (s/class "c-entry-content")
    (url-to-hickory url)))
 
 ;re-implement this with a map later
-(defn guardian-retrieve-fragment
-  [hickory]
+(defn guardian-retrieve-fragment [hickory]
   (if (= (type hickory) "clojure.lang.PersistentVector") (do guardian-retrieve-fragment (first hickory)))
   (if (= (type hickory) "clojure.lang.PersistentArrayMap") (do guardian-retrieve-fragment (get hickory :content)) hickory))
 
-(defmacro retrieve-fragment
-  [url identifier value]
+(defmacro retrieve-fragment [url identifier value]
   (list s/select
         (list identifier value)
         (list url-to-hickory url)))
 
 ;;The next step is to add something between hic-this and retrieve-content to find stuff based on tag name/id/class. Read about map, filter, reduce, and apply in the clojure docs.
 
-(defn retrieve-content
-  [hicvec]
-  (-> (flatten hicvec) (last)))
+(defn retrieve-content [hicvec]
+  (-> hicvec (flatten) (last)))
