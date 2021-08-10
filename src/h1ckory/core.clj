217bcdcb41a41b62f07e2cd9a078ff4457f6e11d
Caleb <beerscb@gmail.com>
2021-08-09 20:38:36 -0500

    add frequency map function

--- src/h1ckory/core.clj
+++ src/h1ckory/core.clj
@@ -6,6 +6,8 @@
 
 (use 'hickory.core)
 
+(def commonwords #{"" "the" "of" "and" "a" "to" "in" "is" "you" "that" "it" "he" "was" "for" "on" "are" "as" "with" "his" "they" "I" "at" "be" "this" "have" "from" "or" "one" "had" "by" "word" "but" "not" "what" "all" "were" "we" "when" "your" "can" "said" "there" "use" "an" "each" "which" "she" "do" "how" "their" "if" "will" "up" "other" "about" "out" "many" "then" "them" "these" "so" "some" "her" "would" "make" "like" "him" "into" "time" "has" "look" "two" "more" "write" "go" "see" "number" "no" "way" "could" "people" "my" "than" "first" "water" "been" "call" "who" "oil" "its" "now" "find" "long" "down" "day" "did" "get" "come" "made" "may" "part"})
+
 (defn current-date []
   (.format 
     (new java.text.SimpleDateFormat "yyyy-MM")
@@ -53,3 +55,14 @@
        (retrieve-text hickory-struct s/tag :a)
        (all-clean-text)))
 
+(defn freq-map [gotten-text]
+  (->> gotten-text
+       (re-seq #"[\w|’|']*")
+       (map clojure.string/lower-case)
+       (remove commonwords)
+       (frequencies)
+       ))
+;these are just some pre-defined values for use in the REPL when testing/developing
+(def url "https://www.nytimes.com/2021/07/24/us/covid-vaccine-hesitant.html")
+(def hick-struct (url-to-hickory url))
+(def scraped (get-text hick-struct))
